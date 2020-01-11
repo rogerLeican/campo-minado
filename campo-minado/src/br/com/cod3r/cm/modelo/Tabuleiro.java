3a5d7a01bc4f1fca150c8134d86273af64fe7d82
rogerLeican <roger.lucian.maciel@gmail.com>
2020-01-11 20:13:27 -0300

    implementação dos metodos gerar campos e associarVizinhos

--- campo-minado/src/br/com/cod3r/cm/modelo/Tabuleiro.java
+++ campo-minado/src/br/com/cod3r/cm/modelo/Tabuleiro.java
@@ -1,6 +1,54 @@
 package br.com.cod3r.cm.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tabuleiro {
+	
+	private int quantidadeLinhas;
+	private int quantidadeColunas;
+	private int quantidadeMinas;
+	
+	
+	private final List<Campo> campos = new ArrayList<>();
+
+
+	public Tabuleiro(int quantidadeLinhas, int quantidadeColunas, int quantidadeMinas) {
+		this.quantidadeLinhas = quantidadeLinhas;
+		this.quantidadeColunas = quantidadeColunas;
+		this.quantidadeMinas = quantidadeMinas;
+		
+		gerarCampos();
+		associarOsVizinhos();
+		sortearMinas();
+	}
+
+	private void gerarCampos() {
+		for (int linha = 0; linha < quantidadeLinhas; linha++) {
+			for (int coluna = 0; coluna < quantidadeColunas; coluna++) {
+				campos.add(new Campo(linha,coluna));
+				
+			}
+		}
+	}
+	
+	private void associarOsVizinhos() {
+		for (Campo c1 : campos) {
+			for (Campo c2 : campos) {
+				c1.adicionarVizinho(c2);
+			}
+		}
+		
+	}
+
+	private void sortearMinas() {
+		
+	}
+
+
+
+
+	
 	
 	
 }
