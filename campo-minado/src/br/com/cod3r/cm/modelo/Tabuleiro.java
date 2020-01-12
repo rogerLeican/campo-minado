4c3c395bbeab8d0a900700b9199de9c1bd08d218
rogerLeican <roger.lucian.maciel@gmail.com>
2020-01-12 12:25:20 -0300

    imports

--- campo-minado/src/br/com/cod3r/cm/modelo/Tabuleiro.java
+++ campo-minado/src/br/com/cod3r/cm/modelo/Tabuleiro.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
+import br.com.cod3r.cm.excecao.ExplosaoException;
+
 public class Tabuleiro {
 	
 	private int quantidadeLinhas;
@@ -25,10 +27,16 @@ public class Tabuleiro {
 	}
 	
 	public void abrir(int linha, int coluna) {
-		campos.parallelStream()
+		try {
+			campos.parallelStream()
 			.filter(c -> c.getLinha()== linha && c.getColuna() == coluna)
 			.findFirst()
 			.ifPresent(c -> c.abrir());;
+		} catch (ExplosaoException e) {
+			campos.forEach(c -> c.setAberto(true));
+			throw e;
+			
+		}
 	}
 	
 	public void AlterarMarcacao(int linha, int coluna) {
@@ -64,9 +72,9 @@ public class Tabuleiro {
 		
 		do {
 			
-			minasArmadas = campos.stream().filter(minado).count();
-			int aleatorio = (int)(Math.random()* campos.size());
+			int aleatorio = (int)(Math.random() * campos.size());
 			campos.get(aleatorio).minar();
+			minasArmadas = campos.stream().filter(minado).count();
 			
 		}while(minasArmadas < quantidadeMinas);
 	}
